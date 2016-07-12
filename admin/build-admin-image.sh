05bb57ba2c18e1841aa53b6250546c7c323dc84f
D.S. Ljungmark <ljungmark@modio.se>
2016-07-12 10:39:37 +0200

    docker: Move to php-fpm as in production
    Currently the two containers now replace the various end ports of things in
    docker, allowing us to keep current nginx configuration and instead replace
    only the unix socket with a tcp socket for php-fpm.

---
+++ admin/build-admin-image.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -eux
+
+REPO_NAME="registry.gitlab.com/modioab/base-image"
+PROJECT="admin"
+RELEASE_TAG="$(date +%Y%m%dT%H%M)"
+
+docker build --tag="${REPO_NAME}:${PROJECT}" \
+             --tag="${REPO_NAME}:${PROJECT}-${RELEASE_TAG}" \
+             --tag="${REPO_NAME}:${PROJECT}-latest" \
+             --file Dockerfile .
+docker push "${REPO_NAME}:${PROJECT}-latest"
+docker push "${REPO_NAME}:${PROJECT}-${RELEASE_TAG}"
