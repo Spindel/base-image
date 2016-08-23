c6e0c847992e4817ec22ef67add7940ea18aa073
D.S. Ljungmark <ljungmark@modio.se>
2016-08-23 21:36:03 +0200

    Clean out debootstrap script a bit

--- debian-armv7/generate-arm-rootfs.sh
+++ debian-armv7/generate-arm-rootfs.sh
@@ -10,17 +10,9 @@ CWD=$(pwd)
 TMPDIR=$(mktemp -d rootfs.XXXXX)
 trap "chmod -R u+w $TMPDIR; rm -rf $TMPDIR" EXIT
 
+PACKAGES="multistrap,squashfs-tools,dracut,xdelta3,unzip,xz-utils,dbus,util-linux,e2fsprogs,dosfstools,tar,make,git,automake,libtool"
 # Create basic root
-debootstrap jessie $TMPDIR
-
-# Install extra deps for modio-distribution
-chroot $TMPDIR apt-get update
-chroot $TMPDIR apt-get -qq -y install multistrap squashfs-tools dracut      \
-                                      xdelta3 unzip xz-utils dbus           \
-                                      util-linux e2fsprogs dosfstools tar   \
-                                      make git automake libtool
-
-chroot $TMPDIR apt-get clean
+debootstrap --include=${PACKAGES} jessie $TMPDIR
 
 # create an archive
 pushd $TMPDIR
