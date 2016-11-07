1f3880dae31726e1f75b2214715f6396cd99823f
D.S. Ljungmark <ljungmark@modio.se>
2016-11-07 15:02:40 +0100

    debian: Add CA-certificates to our debian image

--- debian-armv7/generate-arm-rootfs.sh
+++ debian-armv7/generate-arm-rootfs.sh
@@ -10,7 +10,7 @@ CWD=$(pwd)
 TMPDIR=$(mktemp -d rootfs.XXXXX)
 trap "chmod -R u+w $TMPDIR; rm -rf $TMPDIR" EXIT
 
-PACKAGES="multistrap,squashfs-tools,dracut,xdelta3,unzip,xz-utils,dbus,util-linux,e2fsprogs,dosfstools,tar,make,git,automake,libtool"
+PACKAGES="multistrap,squashfs-tools,dracut,xdelta3,unzip,xz-utils,dbus,util-linux,e2fsprogs,dosfstools,tar,make,git,automake,libtool,ca-certificates"
 # Create basic root
 debootstrap --include=${PACKAGES} jessie $TMPDIR
 
