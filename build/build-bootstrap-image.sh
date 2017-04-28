0dd54ae0c127bdd4044774149671df0eb54587e1
Nicklas Lindgren <nili@gulmohar.se>
2017-04-28 16:09:32 +0200

    Include more build dependencies in bootstrap image
    This change removes the dockersource yum repo, since it was not actually
    used.

--- build/build-bootstrap-image.sh
+++ build/build-bootstrap-image.sh
@@ -9,7 +9,17 @@ if [ $(id -u) != 0 ]; then
   exit 1
 fi
 
-PACKAGES="dnf make supermin docker-engine git"
+PACKAGES=" \
+    dnf \
+    make \
+    supermin \
+    docker-engine \
+    git \
+    xorg-x11-server-Xvfb \
+    firefox \
+    chromium \
+    nodejs \
+    yarn"
 
 source /etc/os-release
 
@@ -17,15 +27,38 @@ mkdir -p build/rootfs/etc/yum.repos.d
 cd build
 ROOTFS=$(realpath rootfs)
 
-cat > rootfs/etc/yum.repos.d/docker.repo <<EOF
-[docker]
-baseurl = https://yum.dockerproject.org/repo/main/fedora/\$releasever/
-gpgcheck = 1
-gpgkey = https://yum.dockerproject.org/gpg
-name = Docker repository
-EOF
+curl -o /etc/yum.repos.d/yarn.repo https://dl.yarnpkg.com/rpm/yarn.repo
 
-dnf --installroot=${ROOTFS} --repofrompath=dockerx,rootfs/etc/yum.repos.d/docker.repo --disablerepo='*' --enablerepo=fedora --enablerepo=updates --enablerepo=dockerx --releasever=${VERSION_ID} --setopt=tsflags=nodocs install -y ${PACKAGES}
+dnf --installroot=${ROOTFS} \
+    --disablerepo='*' \
+    --enablerepo=fedora \
+    --enablerepo=updates \
+    --releasever=${VERSION_ID} \
+    --setopt=tsflags=nodocs \
+    install -y \
+    redhat-release
+
+rpm --root=${ROOTFS} \
+    -i \
+    https://rpm.nodesource.com/pub_4.x/fc/25/x86_64/nodesource-release-fc25-1.noarch.rpm
+
+rpm --root=${ROOTFS} \
+    --import \
+    rootfs/etc/pki/rpm-gpg/NODESOURCE-GPG-SIGNING-KEY-EL
+
+dnf --installroot=${ROOTFS} \
+    --repofrompath=nodesource_x,https://rpm.nodesource.com/pub_6.x/fc/25/x86_64 \
+    --repofrompath=yarn_x,https://dl.yarnpkg.com/rpm/ \
+    --disablerepo='*' \
+    --enablerepo=fedora \
+    --enablerepo=updates \
+    --enablerepo=nodesource_x \
+    --enablerepo=yarn_x \
+    --releasever=${VERSION_ID} \
+    --setopt=tsflags=nodocs \
+    install -y \
+    ${PACKAGES}
+
 echo tsflags=nodocs >> rootfs/etc/dnf/dnf.conf
 
 dnf --installroot=${ROOTFS} clean all
