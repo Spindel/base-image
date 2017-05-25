9473d2b8a6fba17e09723fac5c8cd24d49c4880d
D. Spindel Ljungmark <spider@skuggor.se>
2017-05-25 16:15:02 +0000

    Explicitly add pip

--- python3-image/build-python-rootfs.sh
+++ python3-image/build-python-rootfs.sh
@@ -12,6 +12,7 @@ PACKAGES="python3 \
     python3-pyOpenSSL \
     python3-zope-interface \
     python3-CacheControl \
+    python3-pip \
     python3-netifaces"
 REPO_NAME="registry.gitlab.com/modioab/base-image:${ID}-${VERSION_ID}-${NAME}"
 RELEASE_TAG="-$(date +%Y%m%dT%H%M)"
