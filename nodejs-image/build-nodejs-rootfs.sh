c052b5c8c6f05e31760705e0c14a77c13e0fe816
D.S. Ljungmark <ljungmark@modio.se>
2017-06-13 16:09:48 +0200

    nodejs and meteor base containers
    A build container with upstream meteor (bleugh)
    
    A runtime container with nodejs

---
+++ nodejs-image/build-nodejs-rootfs.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -eux
+
+source /etc/os-release
+OUTPUT="$(pwd)/rootfs.tar"
+
+NAME=nodejs
+PACKAGES="nodejs \
+    npm
+    gzip
+    tar"
+
+REPO_NAME="registry.gitlab.com/modioab/base-image:${ID}-${VERSION_ID}-${NAME}"
+RELEASE_TAG="-$(date +%Y%m%dT%H%M)"
+
+TMPDIR=$(mktemp -d supermin-docker.XXXXX)
+trap "chmod -R u+w $TMPDIR; rm -rf $TMPDIR" EXIT
+
+SUPERMIN_APPLIANCE="${TMPDIR}/${NAME}.o"
+DOCKER_CONTEXT="${TMPDIR}/${NAME}.docker"
+
+dnf install -y ${PACKAGES}
+supermin --prepare ${PACKAGES} -o "${SUPERMIN_APPLIANCE}"
+
+# Exclude Python 2 mystery files
+cat > "${SUPERMIN_APPLIANCE}/excludefiles" <<EOF
+-/usr/bin/python2
+-/usr/bin/python2.7
+EOF
+
+supermin --build --format chroot "${SUPERMIN_APPLIANCE}" -o "${DOCKER_CONTEXT}" --include-packagelist
+pushd ${DOCKER_CONTEXT}
+tar cf ${OUTPUT} .
+popd
